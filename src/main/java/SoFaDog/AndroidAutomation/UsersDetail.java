7601aa38ba60d624d9c81ce8d9eb05b472461893
Kamal Jhinjer <kpsj84>
2022-06-27 11:38:57 +0530

    sofadog browserstack finetune 3

--- src/main/java/SoFaDog/AndroidAutomation/UsersDetail.java
+++ src/main/java/SoFaDog/AndroidAutomation/UsersDetail.java
@@ -3,14 +3,14 @@ package SoFaDog.AndroidAutomation;
 public class UsersDetail {
 	
 	//Change these credentials to run the test's with new Users
-	public static String RegisterTestNumber = "63";
+	public static String RegisterTestNumber = "64";
 	
-	public static String DeleteAccountTestNumber = "111";
+	public static String DeleteAccountTestNumber = "121";
 	
-	public static String BecomeCreatorTestNumber = "208";
+	public static String BecomeCreatorTestNumber = "209";
 	
-	public static String SubscribeFreeChannelTestNumber = "311";
+	public static String SubscribeFreeChannelTestNumber = "312";
 	
-	public static String SubscribePaidChannelTestNumber = "401";
+	public static String SubscribePaidChannelTestNumber = "402";
 	
 }
